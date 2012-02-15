4b5e769bfffa8d4eed3a63279f2f56fdc1f67d6c
Stuart Powers <stuart.powers@gmail.com>
2012-02-15 05:49:29 -0500

    added scrapelogging.py

---
+++ crutchfield.com/scrapelogging.py
@@ -0,0 +1,41 @@
+import logging
+
+
+def get_scrape_logger(name=''):
+
+    # set up logging to file - see previous section for more details
+    logging.basicConfig(level=logging.DEBUG,
+                        format='%(asctime)s\t%(levelname)s\t%(name)s\t%(message)s',
+                        datefmt='%F_%H:%M',
+                        filename='logs/raw_scrape.log',
+                        filemode='a')
+    # define a Handler which writes INFO messages or higher to the sys.stderr
+    console = logging.StreamHandler()
+    console.setLevel(logging.DEBUG),
+
+    # set a format which is simpler for console use
+    formatter = logging.Formatter('%(name)s: %(levelname)s %(message)s')
+
+    # tell the handler to use this format
+    console.setFormatter(formatter)
+
+    # add the handler to the root logger
+    logging.getLogger(name).addHandler(console)
+
+    return logging.getLogger(name)
+
+
+#
+## Now, we can log to the root logger, or any other logger. First the root...
+#logging.info('Jackdaws love my big sphinx of quartz.')
+#
+## Now, define a couple of other loggers which might represent areas in your
+## application:
+#
+#logger1 = logging.getLogger('myapp.area1')
+#logger2 = logging.getLogger('myapp.area2')
+#
+#logger1.debug('Quick zephyrs blow, vexing daft Jim.')
+#logger1.info('How quickly daft jumping zebras vex.')
+#logger2.warning('Jail zesty vixen who grabbed pay from quack.')
+#logger2.error('The five boxing wizards jump quickly.')
